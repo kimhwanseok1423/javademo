c91fd24f4a49da31e7328f2f0c76e2ac279265d2
kimhwanseok <kcsss4499@gmail.com>
2022-10-12 10:38:29 +0900

    update

---
+++ src/java005_method/Java064_method.java
@@ -0,0 +1,41 @@
+package java005_method;
+
+/*
+ * [출력결과]
+ * java test
+ * tset avaj
+ * java test
+ */
+public class Java064_method {
+
+	public static void main(String[] args) {
+		char[] arr = {'j','a','v','a',' ', 't','e','s','t'};
+	      System.out.println(arr);
+	      System.out.println(reverse(arr));
+	      System.out.println(arr);
+
+	}//end main()
+	
+	//shallow copy : 주소복사
+	//deep copy: 요소 복사
+	public static char[] reverse(char[] data) {
+		//data의 요소 앞뒤를 바꿔서 리턴하는 프로그램 구현
+		char[] arr = new char[data.length];
+		//arr[0] = data[8];
+		//arr[1] = data[7];		
+		//arr[8] = data[0];
+		
+		for(int i=0; i<data.length ;i++) {
+			arr[i] = data[data.length-1-i];
+		}				
+		
+		return arr;
+	}//end reverse()
+
+}//end class
+
+
+
+
+
+
